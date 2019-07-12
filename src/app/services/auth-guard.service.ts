a380cc3521efdfe64fd5d6a87afd5aaafe14490e
Luis <luisslillo_90@hotmail.es>
2019-07-12 18:39:20 +0200

    added new error when user want to access to the chat but he is not logged

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { UserProfileService } from './user-profile.service';
+import Swal from 'sweetalert2';
+
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +18,11 @@ export class AuthGuardService implements CanActivate {
     if (this._userService.userlogged) {
       return true;
     } else {
+      Swal.fire({
+        title: 'Please sign into your account to use the chat service.',
+        type: 'error',
+        confirmButtonText: 'Cool'
+      })
       return false;
     }
   }
